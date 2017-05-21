82bd921e5d2812023513fc6284b2fa948de082ea
Code Day <CodeDay@e1z1r2p31.42.us.org>
2017-05-20 17:23:07 -0700

    enemy prefab follow path

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	private GameObject [] waypoints;
+	private int currentWaypoint;
+	private GameObject nextWaypoint;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		currentWaypoint = 0;
+		waypoints = GameObject.FindGameObjectsWithTag ("Waypoint").OrderBy( go => go.name ).ToArray();
+		transform.position = waypoints [currentWaypoint].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentWaypoint < waypoints.Length) {
+			if (nextWaypoint == null) {
+				nextWaypoint = waypoints[currentWaypoint];
+			}
+			moveToNextWaypoint ();
+		}
+	}
+
+	void moveToNextWaypoint () {
+		transform.position = Vector3.MoveTowards(transform.position, nextWaypoint.transform.position, speed*Time.deltaTime);
+		if (transform.position == nextWaypoint.transform.position) {
+			nextWaypoint = waypoints [++currentWaypoint];
+		}
+	}
+
+
+
+}	//class
---
+++ Assets/Scripts/TowerBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBehavior : MonoBehaviour {
+
+	private Ray ray;
+	private RaycastHit hit;
+	private bool toActivate = false;
+	private bool toDeactivate = false;
+	public GameObject optionOne;
+	public GameObject optionTwo;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Gets whether left or right mouse is clicked
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		bool mouseClicked = Input.GetMouseButtonDown (0) || Input.GetMouseButtonDown (1);
+
+		// Mouse not on tower by default
+		bool mouseOnTower = false;
+		if (Physics.Raycast (ray, out hit) && hit.collider.tag == "Tower") {
+			mouseOnTower = true;
+		}
+
+		// If clicked and mouse is over the tower, the current tower is selected
+		if (mouseClicked && mouseOnTower) {
+			toActivate = true;
+		} else if (mouseClicked) {
+			toDeactivate = true;
+		}
+
+		// If selected, show its upgrade options
+		if (toActivate) {
+			optionOne.SetActive (true);
+			optionTwo.SetActive (true);
+			toActivate = false;
+		}
+
+		// IF clicked elsewhere, hide its upgrade options again
+		if (toDeactivate) {
+			optionOne.SetActive (false);
+			optionTwo.SetActive (false);
+			toDeactivate = false;
+		}
+
+	}
+}
